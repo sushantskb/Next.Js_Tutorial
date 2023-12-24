520e823f080aded2f0ed0a8ed4905ef283718c19
sushantskb <91365425+sushantskb@users.noreply.github.com>
2023-12-24 13:21:46 +0530

    events, functions and state in Next Js

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,37 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+"use client"; // this needs to be here, to fix the error while passing a event like alert()
+import { useState } from "react";
+import styles from "./page.module.css";
 const page = () => {
+
+  //state
+  const [name, setName] = useState("Rahul");
+
+  // const apple = () => {
+  //   alert("Hello world")
+  // }
+
+  // with parameter
+  const apple = (item) => {
+    alert("Hello " + item);
+  };
+
+  const handleClick = () => {
+    setName("Sushant");
+  }
+
   return (
-    <>
-      Hello World
-    </>
+    <main className={styles.main}>
+      <h1>Events, Functions and State {name}</h1>
+      {/* <button onClick={()=> {alert("Hello Next.js")}}>Click Me</button> */}
+
+      {/* <button onClick={apple}>Click me</button> */}
+
+      {/* <button onClick={() => apple("Sushant")}>Click me</button> */}
+
+      <button onClick={handleClick}>Click Me</button>
+    </main>
   );
-}
+};
 
 export default page;
