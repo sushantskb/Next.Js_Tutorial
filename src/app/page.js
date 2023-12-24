51c6e7f2392ae647e2607d4497c83710638a25d4
sushantskb <91365425+sushantskb@users.noreply.github.com>
2023-12-24 13:00:22 +0530

    installed next.js in the system

--- src/app/page.js
+++ src/app/page.js
@@ -6,8 +6,8 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
+          Get started by Next Js; 
+          <code className={styles.code}> src/app/page.js</code>
         </p>
         <div>
           <a
