d8f09124082f27b679bb72032f17cbdc058f6f57
sushantskb <91365425+sushantskb@users.noreply.github.com>
2023-12-25 09:46:51 +0530

    basics routing

---
+++ src/app/login/page.js
@@ -0,0 +1,7 @@
+const login = () => {
+    return (
+        <h1>Login Page</h1>
+    );
+}
+
+export default login;
--- src/app/page.js
+++ src/app/page.js
@@ -1,49 +1,9 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-"use client"; // this needs to be here, to fix the error while passing a event like alert()
-import { useState } from "react";
-import styles from "./page.module.css";
-const page = () => {
-
-  //state
-  const [name, setName] = useState("Rahul");
-
-  // const apple = () => {
-  //   alert("Hello world")
-  // }
-
-  // with parameter
-  const apple = (item) => {
-    alert("Hello " + item);
-  };
-
-  const handleClick = () => {
-    setName("Sushant");
-  }
-
-  // component inside component
-  const InnerComp = () => {
-    return <h2>I am inner component</h2>;
-  }
-
+const home = ()=> {
   return (
-    <main className={styles.main}>
-      <h1>Events, Functions and State {name}</h1>
-      {/* <button onClick={()=> {alert("Hello Next.js")}}>Click Me</button> */}
-
-      {/* <button onClick={apple}>Click me</button> */}
-
-      {/* <button onClick={() => apple("Sushant")}>Click me</button> */}
-
-      <button onClick={handleClick}>Click Me</button>
-
-      {/* Calling as a component */}
-      {/* <InnerComp /> */}
-
-      {/* Calling as a function */}
-      {InnerComp()}
-
+    <main>
+      <h1>Basics Routing || Making New Page</h1>
     </main>
   );
-};
+}
 
-export default page;
+export default home;
