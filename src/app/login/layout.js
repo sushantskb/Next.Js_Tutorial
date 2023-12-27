aa0fe14089c1e7c3b961b98c733dc16ada062a1f
sushantskb <91365425+sushantskb@users.noreply.github.com>
2023-12-27 19:50:57 +0530

    conditional layout

--- src/app/login/layout.js
+++ src/app/login/layout.js
@@ -1,19 +1,35 @@
+"use client";
 import Link from "next/link";
-import React from "react"
-import "./login.css"
-const Layout = ({children}) => {
-    return (
-        <>
-                
+import React from "react";
+import { usePathname } from "next/navigation";
+import "./login.css";
+const Layout = ({ children }) => {
+  const pathname = usePathname();
+  console.log(pathname);
+  return (
+    <>
+      {pathname !== "/login/studentLogin" ? (
+        <ul className="menu-list">
+          <li className="logo">Login Navabar</li>
+          <li>
+            <Link href="/login">Login Main</Link>
+          </li>
+          <li>
+            <Link href="/login/studentLogin">Student Login</Link>
+          </li>
+        </ul>
+      ) : (
+        <ul className="menu-list">
+          <li className="logo">Login Navabar</li>
+          <li>
+            <Link href="/login/studentLogin">Student Login</Link>
+          </li>
+        </ul>
+      )}
 
-            <ul className="menu-list">
-                <li className="logo">Login Navabar</li>
-                <li><Link href="/login">Login Main</Link></li>
-                <li><Link href="/login/studentLogin">Student Login</Link></li>
-            </ul>
-            {children}
-        </>
-    )
-}
+      {children}
+    </>
+  );
+};
 
 export default Layout;
