687126ecfb293b7c5ffaa5691f8262d576b70045
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-01-03 09:58:13 +0530

    modular css in next.js

--- src/app/productlist/page.js
+++ src/app/productlist/page.js
@@ -1,27 +1,36 @@
 import Price from "./product";
+import custom from "./style/custom.module.css";
 async function fetchData() {
   let data = await fetch("https://dummyjson.com/products");
   data = await data.json();
   return data.products;
 }
 
-const ProductList = async ()=> {
+const ProductList = async () => {
   let product = await fetchData();
   console.log(product);
   return (
     <>
-      <h1>Product List</h1>
-      {
-        product.map((item)=> (
-          <div key={item._id}>
-            <h3>Name : {item.title}</h3>
-            <Price price={item.price} />
-            <hr />
-          </div>
-        ))
-      }
+      <h1 className={custom.h1}>Product List</h1>
+      <table className={custom.table}>
+        <thead>
+          <tr>
+            <th className={custom.th}>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {product.map((item) => (
+            <tr key={item._id}>
+              <td className={custom.td}>{item.title}</td>
+              <td className={custom.td}><Price price={item.price} /></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
-  )
+  );
 }
+
 
 export default ProductList;
