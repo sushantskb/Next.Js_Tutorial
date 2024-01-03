5207cc532c4f4feff5852d05fabe1f80d97687d6
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-01-03 10:17:47 +0530

    conditional css in next js

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,19 @@
+"use client"
 import Link from "next/link";
-
+import style from "./style.module.css";
+import { useState } from "react";
 const Home = () => {
+  const [color, setColor] = useState("red");
+  const handleClick = () => {
+    setColor("color")
+  }
   return <div>
-    <h1>Fetch API data from Server Component</h1>
-    <Link href="/productlist">Go to Product List</Link>
+    <h1 
+      className={color == "red" ? style.red : style.green}
+    >
+      Conditional CSS
+    </h1>
+    <button className={style.btn} onClick={handleClick}>Update Color</button>
   </div>
 }
 
