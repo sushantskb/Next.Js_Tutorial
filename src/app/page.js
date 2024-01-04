fcb4e2ed911c3af9a25c60c90c66ef1ef62e66f2
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-01-04 10:13:21 +0530

    image optimization in next js

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,27 @@
-"use client"
-import Link from "next/link";
-import style from "./style.module.css";
-import { useState } from "react";
+/* eslint-disable jsx-a11y/alt-text */
+import Image from "next/image";
+import Profile from "../../public/vercel.svg";
 const Home = () => {
-  const [color, setColor] = useState("red");
-  const handleClick = () => {
-    setColor("color")
-  }
-  return <div>
-    <h1 
-      className={color == "red" ? style.red : style.green}
-    >
-      Conditional CSS
-    </h1>
-    <button className={style.btn} onClick={handleClick}>Update Color</button>
-  </div>
-}
+  return (
+    <>
+      <h1>Image optimzation</h1>
+      {/* Image tag is a special tag that is used in Next Js or Next Js suggesst us use so that the image can be directly optimized without using any external libarary */}
+      {/* <Image src={Profile} /> */}
+
+      {/* if we use normal html tag then :  */}
+      {/* <img src={Profile.src} /> */}
+
+      <hr />
+
+      <p>external image</p>
+      {/* Note : add the external image's domain in next.config.js */}
+      <Image 
+        src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?w=1380&t=st=1704343012~exp=1704343612~hmac=3fe909ffcb4f3c16206592d8ef823591e931b81388a3acd72a800c06c70b0d3f" 
+        width={1380}
+        height={789}
+      />
+    </>
+  );
+};
 
 export default Home;
