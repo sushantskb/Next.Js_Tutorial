e6c8be55ab8f38617ca48c53d9eef3642cc9d405
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-02-09 23:47:23 +0530

    Make API with Post Method Next js 13.4

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -5,3 +5,9 @@ export function GET(){
     const data = user;
     return NextResponse.json(data, {status: 200});
 }
+
+export async function POST(request){
+    let payload = await request.json();
+    console.log(payload);
+    return NextResponse.json({result:payload.name})
+}
