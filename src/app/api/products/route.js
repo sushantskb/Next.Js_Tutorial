3ede8b109f7f0e747514ea2b13faa8ff4b21a376
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-02-18 10:49:43 +0530

    POST API with MongoDB in Next.js 13.4

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -8,3 +8,11 @@ export async function GET(){
     console.log(data);
     return NextResponse.json({result : data})
 }
+
+export async function POST(req){
+    const data = await req.json();
+    await mongoose.connect(connectionString).then(console.log("Connected")).catch((err)=>console.log(err));
+    let product = new Product(data);
+    const result = await product.save();
+    return NextResponse.json({ result, success: true});
+}
