148bbf09037381b4dd340ed10d89a46973bd232f
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-03-15 01:02:21 +0530

    File Upload in Next Js

--- src/app/api/[...student]/route.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET(request, content){
-    // console.log(content);
-    const studentDetails = content.params.student;
-    console.log(studentDetails);
-    return NextResponse.json({result: studentDetails, success: true}, {status: 201});
-}
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,37 @@
+"use client"
 import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Home() {
+  const [file, setFile] = useState();
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log(file);
+    const data = new FormData();
+    data.set('file', file);
+    let result = await fetch("api/upload", {
+      method: 'POST',
+      body: data,
+    });
+    result = await result.json();
+    console.log(result);
+    if(result.success){
+      alert("file uploaded")
+    }
+  }
   return (
     <main className={styles.main}>
-      <Link href="/addProduct">Add Product</Link>
-      <Link href="/products">Products</Link>
+      <h1>Upload Image</h1>
+      <form onSubmit={onSubmit} >
+        <input 
+          type="file" 
+          name="file"
+          onChange={(e) => setFile(e.target.files?.[0])}
+        />
+        <button type="submit">Upload Image</button>
+      </form>
     </main>
   );
 }
