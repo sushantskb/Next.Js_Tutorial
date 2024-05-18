44683f02893c6299697aa634bdeb0cf65e191682
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-05-18 22:34:12 +0530

    Added the header section and google fonts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,14 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "./components/Header";
+import { Mulish } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const mulish = Mulish({
+  weight:"400",
+  subsets: ["latin"],
+  display: "swap"
+})
+
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +18,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={mulish.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
