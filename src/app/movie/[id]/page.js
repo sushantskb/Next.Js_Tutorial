c5838fadcdf22c1674ee4d376b24ed90ce5f3d61
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-05-19 19:25:13 +0530

    created the dynamic routing using Api for movis pg

---
+++ src/app/movie/[id]/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <h1>The dynamic page</h1>
+  )
+}
+
+export default page
--- src/app/movie/page.js
+++ src/app/movie/page.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import MovieCard from "../components/MovieCard";
+import styles from "@/app/styles/common.module.css";
+const Movie = async () => {
+  const url = process.env.RAPID_KEY;
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "928c7d5572msh247d9e9568826d2p1f42cbjsndb35fd5c32ff",
+      "X-RapidAPI-Host": "netflix54.p.rapidapi.com",
+    },
+  };
 
-const Page = () => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+  // console.log(data);
+  const main_data = data.titles;
+
   return (
-    <div>Movie Page</div>
-  )
-}
+    <section className={styles.movieSection}>
+      <div className={styles.container}>
+        <h1>Series and Movies</h1>
+        {main_data.map((item) => {
+          return <MovieCard key={item.id} item = {item}/>;
+        })}
+      </div>
+    </section>
+  );
+};
 
-export default Page
+export default Movie;
