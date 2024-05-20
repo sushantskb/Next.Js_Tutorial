10338abfbbe876ef4ed094c15e256829cae75ae2
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-05-21 00:05:34 +0530

    added the contact card to contact us page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import ContactCard from '../components/ContactCard'
+import styles from "@/app/contact/contact.module.css"
 const Page = () => {
   return (
-    <div>Contact Us</div>
+    <div className={styles.container}>
+      <h1>Contact Page</h1>
+      <ContactCard />
+    </div>
   )
 }
 
