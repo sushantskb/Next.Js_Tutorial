ed4d5a022dea2fdd4173ce26976c47309c6e8606
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-05-20 18:40:44 +0530

    added the loader in site

--- src/app/movie/page.js
+++ src/app/movie/page.js
@@ -3,6 +3,7 @@ import React from "react";
 import MovieCard from "../components/MovieCard";
 import styles from "@/app/styles/common.module.css";
 const Movie = async () => {
+  await new Promise(resolve => setTimeout(resolve, 2000))
   const url = process.env.RAPID_KEY;
   const options = {
     method: "GET",
